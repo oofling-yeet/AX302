1eebe94dc80113c2be1ff49133086052139f3e6c
oofling-yeet <annawowpplyaya@gmail.com>
2019-03-02 11:58:28 +0800

    update

--- lesson8/app.js
+++ lesson8/app.js
@@ -79,4 +79,25 @@ function update(){
 	game.physics.arcade.collide(stars,platforms);
 	game.physics.arcade.collide(enemy1,platforms);
 
+	player.body.velocity.x = 0;
+
+	if (cursors.left.isDown){
+		player.body.velocity.x = -150;
+		player.animations.play('left');
+	}
+
+	else if (cursor.right.isDown){
+		player.body.velocity.x = 150;
+		player.animations.play('right');
+	}
+
+	else {
+		player.animations.stop();
+		player.frame = 4;
+	}
+
+	if (cursors.up.isDown && player.body.touching.down){
+		player.body.velocity.y = -300;
+	}
+
 }
